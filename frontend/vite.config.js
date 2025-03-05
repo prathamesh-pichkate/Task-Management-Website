73a79e9f073b4a8ba4b7f4dec9ea82609937a1b7
prathamesh-pichkate <prathameshpichkate@gmail.com>
2025-03-05 17:50:40 +0530

    updated the file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+// import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +11,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  // plugins: [react()],
 });
